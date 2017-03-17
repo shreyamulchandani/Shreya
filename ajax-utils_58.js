58d3b8f09d12f211e9e42b689f84c77561634cde
shreyamulchandani <shreya.mulchandani@ymail.com>
2017-03-17 10:03:49 +0530

    Add files via upload

---
+++ ajax-utils_58.js
@@ -0,0 +1,19 @@
+(function (global) {
+
+// Set up a namespace for our utility
+var ajaxUtils = {};
+
+
+// Returns an HTTP request object
+function getRequestObject() {
+  if (global.XMLHttpRequest) {
+    return (new XMLHttpRequest());
+  } 
+  else if (global.ActiveXObject) {
+    // For very old IE browsers (optional)
+    return (new ActiveXObject("Microsoft.XMLHTTP"));
+  } 
+  else {
+    global.alert("Ajax is not supported!");
+    return(null); 
+  }
