d131f053860b93594a80d1ec9954709da1bdccf0
shreyamulchandani <shreya.mulchandani@ymail.com>
2017-03-24 10:18:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+function onLoad(){
+	var name = document.getElementById("name").value;
+	// console.log(name);
+	var xhttp = new XMLHttpRequest;
+	xhttp.onreadystatechange = function(){
+		if (this.readyState == 4 && this.status ==200 ) {
+			var jsonObj = JSON.parse(xhttp.responseText);
+			var flag = 1;
+			// console.log(jsonObj.length);
+			for (var i = 0; i < jsonObj.length; i++) {
+				if (jsonObj[i].name == name) {
+					document.getElementById("demo").innerHTML = "Hello "+name+",<br>WPtheory Attendance : "+jsonObj[i].wp+"<br>WPlab Attendance : "+jsonObj[i].mpmc;
+					break;
+				}else{
+					flag = 0;
+				}
+				if (flag == 0) {
+					document.getElementById("demo").innerHTML = "Incorrect Name";	
+				}
+			}
+		}
+	};
+	xhttp.open("GET","data.json",true);
+	xhttp.send(null);
+}
+
+
+
